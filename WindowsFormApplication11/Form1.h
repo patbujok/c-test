53b23071a81fa12f2359f414e0c172cec0bf4b54
patbujok <pat.bujok@gmail.com>
2015-05-11 08:53:39 +0200

    Dodano przycisk konieci fucnkcje zwiazana z tym przyciskiem.

--- WindowsFormApplication11/Form1.h
+++ WindowsFormApplication11/Form1.h
@@ -34,6 +34,8 @@ namespace WindowsFormApplication1 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^  button1;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,19 +50,34 @@ namespace WindowsFormApplication1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(95, 260);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(250, 42);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Koniec";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
 			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(440, 355);
+			this->Controls->Add(this->button1);
 			this->Name = L"Form1";
 			this->Text = L"Form1";
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+		Close();
+	}
 	};
 }
 
